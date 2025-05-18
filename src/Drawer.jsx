abd86c988fc5ff80cf9f11196269dea46f98f854
deep0ctave <130386277+deep0ctave@users.noreply.github.com>
2025-05-18 21:10:21 +0530

    Add Navigation functionality to Sidebar

--- src/Drawer.jsx
+++ src/Drawer.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom';
 import Navbar from './Navbar'
 
 function Drawer({ showSidebar, children }) {
+
+  const getLinkClasses = ({ isActive }) =>
+    isActive
+      ? 'bg-primary text-white font-semibold rounded-md'
+      : 'hover:bg-base-200 rounded-md';
+
   return (
     <div className={`drawer ${showSidebar ? 'lg:drawer-open' : ''} overflow-hidden h-screen w-screen`}>
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
@@ -12,16 +19,16 @@ function Drawer({ showSidebar, children }) {
       </div>
       {showSidebar && (
         <div className="drawer-side lg:hidden h-full z-40 lg:z-30">
-            <div class="bg-base-300 navbar sticky top-0 z-20 hidden items-center gap-2 px-4 py-0 backdrop-blur flex-1 text-xl font-bold lg:flex "><h1>Telvy</h1></div>
+            <div className="bg-base-300 navbar sticky top-0 z-20 hidden items-center gap-2 px-4 py-0 backdrop-blur flex-1 text-xl font-bold lg:flex "><h1>Telvy</h1></div>
           <label htmlFor="my-drawer" className="drawer-overlay" />
           {/* Sidebar Content */}
           <ul className="menu p-4 w-80 min-h-full bg-base-300 text-base-content">
-            <li><a>Home</a></li>
-            <li><a>Quizzes</a></li>
-            <li><a>Groups</a></li>
-            <li><a>Notifications</a></li>
-            <li><a>Settings</a></li>
-            <li><a>Help & Feedback</a></li>
+             <li><NavLink to="/home" className={getLinkClasses}>Home</NavLink></li>
+            <li><NavLink to="/quiz" className={getLinkClasses}>Quizzes</NavLink></li>
+            <li><NavLink to="/groups" className={getLinkClasses}>Groups</NavLink></li>
+            <li><NavLink to="/notifications" className={getLinkClasses}>Notifications</NavLink></li>
+            <li><NavLink to="/settings" className={getLinkClasses}>Settings</NavLink></li>
+            <li><NavLink to="/help" className={getLinkClasses}>Help & Feedback</NavLink></li>
           </ul>
         </div>
       )}
